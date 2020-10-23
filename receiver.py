bdde3af682bddf7d31618fbe74412f30277122e0
Leonel Andrés Polanía Arias <la.polania@uniandes.edu.co>
2020-10-22 19:25:33 -0500

    Implementa envío Multicast

---
+++ receiver.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+from __future__ import division
+import cv2
+import numpy as np
+import socket
+import struct
+
+MAX_DGRAM = 2**16
+
+def dump_buffer(s):
+    """ Emptying buffer frame """
+    while True:
+        seg, addr = s.recvfrom(MAX_DGRAM)
+        print(seg[0])
+        if struct.unpack("B", seg[0:1])[0] == 1:
+            print("finish emptying buffer")
+            break
+
+def main():
+    """ Getting image udp frame &
+    concate before decode and output image """
+    
+    # Set up socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    multicast_group = '224.3.29.71'
+    s.bind(('', 10000))
+
+    group = socket.inet_aton(multicast_group)
+    mreq = struct.pack('4sL', group, socket.INADDR_ANY)
+    s.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+
+    dat = b''
+    dump_buffer(s)
+
+    while True:
+        seg, addr = s.recvfrom(MAX_DGRAM)
+        if struct.unpack("B", seg[0:1])[0] > 1:
+            dat += seg[1:]
+        else:
+            dat += seg[1:]
+            img = cv2.imdecode(np.fromstring(dat, dtype=np.uint8), 1)
+            cv2.imshow('frame', img)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            dat = b''
+
+    # cap.release()
+    cv2.destroyAllWindows()
+    s.close()
+
+if __name__ == "__main__":
+    main()
